fe01e6db3017dec53a52765b34f396213a258416
jdfiallos <juandanielfiallos@gmail.com>
2021-06-05 13:38:49 -0600

    [NEW] openacademy

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """Manage trainings""",
+
+    'description': """ 
+        Open Academy modules for managing trainings:
+            - training courses
+            - training sessions
+            -attendees registration
+    """,
+
+    'author': "My Company",
+    'website': "http://www/yourcompany.com",
+
+    'category' : 'Test',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data' : [
+        'views.xml',
+    ],
+    
+    'demo' : [
+        'demo.xml'
+    ],
+}
