a5e7f04f40e307f10d9523d4dee031a57e287363
jdfiallos <juandanielfiallos@gmail.com>
2021-06-07 00:58:39 +0000

    [ADD] relationship

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -8,5 +8,6 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
 
-    responsible_id = fields.Many2one('res.users',
-    ondelete='set null', string="Responsible", index=True)
+    responsible_id = fields.Many2one('res.users', 
+        ondelete='set null', string="Responsible", index=True)
+    session_id = fields.One2many('openacademy.session', 'course_id', string="Sessions")
